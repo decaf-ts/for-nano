5ffb758a1c3cf033376bc500b1421763c0698229
tvenceslau <tiago.venceslau@pdmfc.com>
2025-05-23 11:52:15 +0100

    DECAF-126 working nano

--- tests/integration/setup.ts
+++ tests/integration/setup.ts
@@ -1,7 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { IntegrationController } from "./IntegrationController";
 
 export default async function setup(): Promise<void> {
-  // await IntegrationController.loginDocker();
-  // await IntegrationController.setupBackup();
+  if (process.env["CI"]) {
+    await IntegrationController.loginDocker();
+    await IntegrationController.setupBackup();
+  }
 }
--- tests/integration/teardown.ts
+++ tests/integration/teardown.ts
@@ -1,6 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { IntegrationController } from "./IntegrationController";
 
 export default async function teardown(): Promise<void> {
-  // await IntegrationController.dropBackup();
+  if (process.env["CI"]) await IntegrationController.dropBackup();
 }
